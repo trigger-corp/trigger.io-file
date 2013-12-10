9ac7b702a8a14594ca7a67477d61f276b90f838d
Connor Dunn <connor@trigger.io>
2013-12-10 17:10:56 +0000

    Remove ignored files

--- inspector/ios-inspector/ForgeInspector/assets/src/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-	forge.file.saveURL("http://192.168.2.144/~james/biggish.mp4", function (name) {
-		forge.logging.info(JSON.stringify(name) + " done!");
-// 		forge.file.saveURL("http://192.168.2.144/~james/biggish.mp4", function (name) {
-// 			forge.logging.info(JSON.stringify(name) + " done!");
-// 		});
-	});
-});
