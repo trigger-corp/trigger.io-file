390f82f1068245c8ab23de3a8e8f82a56de1e0db
Antoine van Gelder <antoine@trigger.io>
2017-05-18 15:38:34 +0200

    Fix iOS crash when there are no localization strings for the selected device language.

--- inspector/ios-inspector/ForgeModule/file/file_EventListener.m
+++ inspector/ios-inspector/ForgeModule/file/file_EventListener.m
@@ -4,11 +4,18 @@
 @implementation file_EventListener
 
 + (void)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    io_trigger_dialog_capture_camera_description = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"io_trigger_dialog_capture_camera_description"];
-    io_trigger_dialog_capture_source_camera = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"io_trigger_dialog_capture_source_camera"];
-    io_trigger_dialog_capture_source_gallery = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"io_trigger_dialog_capture_source_gallery"];
-    io_trigger_dialog_capture_pick_source = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"io_trigger_dialog_capture_pick_source"];
-    io_trigger_dialog_cancel = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"io_trigger_dialog_cancel"];
+    NSBundle *bundle = [NSBundle mainBundle];
+
+    if ([bundle objectForInfoDictionaryKey:@"io_trigger_dialog_capture_camera_description"])
+        io_trigger_dialog_capture_camera_description = [bundle objectForInfoDictionaryKey:@"io_trigger_dialog_capture_camera_description"];
+    if ([bundle objectForInfoDictionaryKey:@"io_trigger_dialog_capture_source_camera"])
+        io_trigger_dialog_capture_source_camera = [bundle objectForInfoDictionaryKey:@"io_trigger_dialog_capture_source_camera"];
+    if ([bundle objectForInfoDictionaryKey:@"io_trigger_dialog_capture_source_gallery"])
+        io_trigger_dialog_capture_source_gallery = [bundle objectForInfoDictionaryKey:@"io_trigger_dialog_capture_source_gallery"];
+    if ([bundle objectForInfoDictionaryKey:@"io_trigger_dialog_capture_pick_source"])
+        io_trigger_dialog_capture_pick_source = [bundle objectForInfoDictionaryKey:@"io_trigger_dialog_capture_pick_source"];
+    if ([bundle objectForInfoDictionaryKey:@"io_trigger_dialog_cancel"])
+        io_trigger_dialog_cancel = [bundle objectForInfoDictionaryKey:@"io_trigger_dialog_cancel"];
 }
 
 @end
