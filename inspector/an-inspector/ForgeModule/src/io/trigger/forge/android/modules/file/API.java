c5b0204eb6bae2d4e2b99ca075d27865a878024f
Antoine van Gelder <antoine@trigger.io>
2020-09-03 16:45:36 +0200

    Handle image size and rotation options for Android image picker

--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/file/API.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/file/API.java
@@ -2,26 +2,31 @@ package io.trigger.forge.android.modules.file;
 
 import android.Manifest;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.util.Base64;
 import android.webkit.MimeTypeMap;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.MetadataException;
+import com.drew.metadata.exif.ExifIFD0Directory;
 import com.google.gson.JsonObject;
 import com.llamalab.safs.Paths;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.FileChannel;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import io.trigger.forge.android.core.ForgeActivity.EventAccessBlock;
 import io.trigger.forge.android.core.ForgeApp;
@@ -31,6 +36,7 @@ import io.trigger.forge.android.core.ForgeLog;
 import io.trigger.forge.android.core.ForgeParam;
 import io.trigger.forge.android.core.ForgeStorage;
 import io.trigger.forge.android.core.ForgeTask;
+import io.trigger.forge.android.util.BitmapUtil;
 
 import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
@@ -50,25 +56,40 @@ public class API {
     }
 
     private static void pickMedia(final ForgeTask task, final String type) {
+        // parse options
+        final int maxWidth = task.params.has("width") ? task.params.get("width").getAsInt() : 0;
+        final int maxHeight = task.params.has("height") ? task.params.get("height").getAsInt() : 0;
+        final boolean fixRotation = task.params.has("fixRotation") ? task.params.get("fixRotation").getAsBoolean() : false;
+        final String videoQuality = task.params.has("videoQuality") ? task.params.get("videoQuality").getAsString() : "default";
+
         ForgeIntentResultHandler resultHandler = new ForgeIntentResultHandler() {
             @Override
             public void result(int requestCode, int resultCode, Intent data) {
                 if (resultCode == RESULT_OK) {
+
+                    /* TODO if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                        int itemCount = data.getClipData().getItemCount();
+                        for (int index = 0; index < itemCount; index++) {
+                            Uri uri = data.getClipData().getItemAt(index).getUri();
+                            ForgeLog.d(uri.toString());
+                        }
+                    }*/
+
                     Uri source = data.getData();
                     try {
-                        ContentResolver contentResolver = ForgeApp.getActivity().getContentResolver();
+                        ForgeFile forgeFile = null;
+                        if (type.startsWith("image/") && (maxWidth > 0 || maxHeight > 0 || fixRotation == true)) {
+                            int maxWidth = Integer.parseInt(task.params.get("width").getAsString());
+                            int maxHeight = Integer.parseInt(task.params.get("height").getAsString());
+                            forgeFile = writeImageUri(source, fixRotation, maxWidth, maxHeight);
 
-                        String extension = MimeTypeMap.getSingleton().getExtensionFromMimeType(contentResolver.getType(source));
-                        String filename = ForgeStorage.temporaryFileNameWithExtension(extension);
-                        ForgeFile forgeFile = new ForgeFile(ForgeStorage.EndpointId.Temporary, filename);
-                        File destination = Paths.get(ForgeStorage.getNativeURL(forgeFile).getPath()).toFile();
+                        } else if (type.startsWith("video/") && !videoQuality.equalsIgnoreCase("default")) {
+                            // TODO transcode video once min API level hits 18 and we can rely on MediaCodec being present
+                            forgeFile = API.writeMediaUri(source);
 
-                        FileInputStream inputStream = (FileInputStream)contentResolver.openInputStream(source);
-                        FileChannel sourceChannel = inputStream.getChannel();
-                        FileChannel destinationChannel = new FileOutputStream(destination).getChannel();
-                        destinationChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
-                        sourceChannel.close();
-                        destinationChannel.close();
+                        } else {
+                            forgeFile = API.writeMediaUri(source);
+                        }
 
                         task.success(forgeFile.toScriptObject());
 
@@ -93,11 +114,92 @@ public class API {
                     public void run() {
                         Intent intent = new Intent(Intent.ACTION_PICK);
                         intent.setType(type);
+                        // TODO intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
                         ForgeApp.intentWithHandler(intent, resultHandler);
                     }
                 });
             }
         });
+    }
+
+    private static ForgeFile writeMediaUri(Uri source) throws IOException {
+        ContentResolver contentResolver = ForgeApp.getActivity().getContentResolver();
+
+        String extension = MimeTypeMap.getSingleton().getExtensionFromMimeType(contentResolver.getType(source));
+        String filename = ForgeStorage.temporaryFileNameWithExtension(extension);
+        ForgeFile forgeFile = new ForgeFile(ForgeStorage.EndpointId.Temporary, filename);
+        File destination = Paths.get(ForgeStorage.getNativeURL(forgeFile).getPath()).toFile();
+
+        FileInputStream inputStream = (FileInputStream)contentResolver.openInputStream(source);
+        FileChannel sourceChannel = inputStream.getChannel();
+        FileChannel destinationChannel = new FileOutputStream(destination).getChannel();
+        destinationChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
+        sourceChannel.close();
+        destinationChannel.close();
+
+        return forgeFile;
+    }
+
+    private static ForgeFile writeImageUri(Uri source, boolean fixRotation, int maxWidth, int maxHeight) throws IOException {
+        ContentResolver contentResolver = ForgeApp.getActivity().getContentResolver();
+
+        String filename = ForgeStorage.temporaryFileNameWithExtension("jpg");
+        ForgeFile forgeFile = new ForgeFile(ForgeStorage.EndpointId.Temporary, filename);
+        File destination = Paths.get(ForgeStorage.getNativeURL(forgeFile).getPath()).toFile();
+
+        // read source metadata
+        Metadata metadata = null;
+        try {
+            BufferedInputStream inputStream = new BufferedInputStream(contentResolver.openInputStream(source));
+            metadata = ImageMetadataReader.readMetadata(inputStream, false);
+            if (metadata == null) {
+                throw new IOException("Failed to obtain metadata for image with uri: " + source.toString());
+            }
+        } catch (ImageProcessingException e) {
+            throw new IOException(e.getLocalizedMessage());
+        }
+
+        // fix source rotation - TODO optional?
+        int rotation = 0;
+        ExifIFD0Directory exifIFD0Directory = metadata.getDirectory(ExifIFD0Directory.class);
+        if (exifIFD0Directory == null) {
+            ForgeLog.w("No Exif data included in image with uri: " + source.toString());
+        } else {
+            try {
+                switch (exifIFD0Directory.getInt(ExifIFD0Directory.TAG_ORIENTATION)) {
+                    case 1:	 rotation = 0;   break;
+                    case 3:  rotation = 180; break;
+                    case 6:  rotation = 90;  break;
+                    case 8:  rotation = 270; break;
+                    default: rotation = 0;   break;
+                }
+            } catch (MetadataException e) {
+                ForgeLog.w("Failed to extract Exif data from image with uri: " + source.toString());
+            }
+            ForgeLog.d("Setting rotation from Exif data: " + rotation + " degrees");
+        }
+
+        // create an in-memory bitmap object from source uri and resize
+        Context context = ForgeApp.getActivity();
+        Bitmap bitmap = BitmapUtil.bitmapFromUri(context, source);
+        Matrix matrix = BitmapUtil.matrixForBitmap(context, bitmap, maxWidth, maxHeight, rotation);
+        if (matrix == null) {
+            return writeMediaUri(source); // fallback to saving the original image
+        }
+        bitmap = BitmapUtil.applyMatrix(context, bitmap, matrix);
+        if (bitmap == null) {
+            return writeMediaUri(source); // fallback to saving the original image
+        }
+
+        // save in-memory bitmap
+        FileOutputStream outputStream = new FileOutputStream(destination);
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, outputStream);
+        outputStream.close();
+
+        // release bitmap memory
+        bitmap.recycle();
+
+        return forgeFile;
     }
 
     //endregion media picker
